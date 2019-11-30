f41f237e11604191b9f988dee88dc0127bfa4b85
Pavel Shvetsov <pshvetso@mail.ru>
2019-11-30 13:51:20 +0300

    AreaSelect fixed, assets added.

--- src/components/AreaSelect.vue
+++ src/components/AreaSelect.vue
@@ -19,16 +19,15 @@
         name: "AreaSelect",
         data: () => ({
             stage: null,
+            img: null,
+            strokeDash: null,
+            r: null,
+            moveListener: null,
         }),
         mounted() {
-            let stage = new createjs.Stage("canvas");
-            this.stage = stage;
+            this.stage = new createjs.Stage("canvas");
 
-            createjs.Ticker.on("tick", tick);
-            function tick(event) {
-                stage.update(event);
-                sd.offset--;
-            }
+            createjs.Ticker.on("tick", this.tickerOnTick);
 
             let selection = new createjs.Shape(),
                 g = selection.graphics.setStrokeStyle(1).beginStroke("#000").beginFill("rgba(0,0,0,0.2)"),
@@ -36,24 +35,13 @@
                 r = g.drawRect(0,0,100,100).command,
                 moveListener;
 
+            this.stage.on("stagemousedown", this.dragStart);
+            this.stage.on("stagemouseup", this.dragEnd);
 
-            stage.on("stagemousedown", dragStart);
-            stage.on("stagemouseup", dragEnd);
-
-            function dragStart(event) {
-                stage.addChild(selection).set({x:event.stageX, y:event.stageY});
-                r.w = 0; r.h = 0;
-                moveListener = stage.on("stagemousemove", drag);
-            }
-
-            function drag(event) {
-                r.w = event.stageX - selection.x;
-                r.h = event.stageY - selection.y;
-            }
-
-            function dragEnd() {
-                stage.off("stagemousemove", moveListener);
-            }
+            this.selection = selection;
+            this.strokeDash = sd;
+            this.r = r;
+            this.moveListener = moveListener;
         },
         methods: {
             previewImage: function(event) {
@@ -69,40 +57,57 @@
                         // Read image as base64 and set to imageData
                         //this.imageData = e.target.result;
 
-                        let img = new Image();
-                        img.onload = function() {
-                            let bmp = new createjs.Bitmap(img);
-                            this.stage.addChild(bmp);
-
-                            let scale, x = 0, y = 0;
-                            if(bmp.image.height > bmp.image.width) {
-                                scale = this.stage.canvas.height / bmp.image.height;
-                                x = (this.stage.canvas.width - bmp.image.width * scale) /2;
-                            } else {
-                                scale = this.stage.canvas.width / bmp.image.width;
-                                y = (this.stage.canvas.height - bmp.image.height * scale) /2;
-                            }
-
-                            bmp.x = x;
-                            bmp.y = y;
-                            bmp.scaleX = scale;
-                            bmp.scaleY = scale;
-                            this.stage.update();
-                        };
-                        img.src = e.target.result;
+                        this.img = new Image();
+                        this.img.onload = this.onFileUpload;
+                        this.img.src = e.target.result;
                     };
                     // Start the reader job - read file as a data url (base64 format)
                     reader.readAsDataURL(input.files[0]);
                 }
-            }
+            },
+            onFileUpload: function() {
+                let bmp = new createjs.Bitmap(this.img);
+                this.stage.addChild(bmp);
+
+                let scale, x = 0, y = 0;
+                if(bmp.image.height > bmp.image.width) {
+                    scale = this.stage.canvas.height / bmp.image.height;
+                    x = (this.stage.canvas.width - bmp.image.width * scale) /2;
+                } else {
+                    scale = this.stage.canvas.width / bmp.image.width;
+                    y = (this.stage.canvas.height - bmp.image.height * scale) /2;
+                }
+
+                bmp.x = x;
+                bmp.y = y;
+                bmp.scaleX = scale;
+                bmp.scaleY = scale;
+                this.stage.update();
+            },
+            tickerOnTick: function() {
+                this.stage.update(event);
+                this.strokeDash.offset--;
+            },
+            dragStart: function(event) {
+                this.stage.addChild(this.selection).set({x:event.stageX, y:event.stageY});
+                this.r.w = 0;
+                this.r.h = 0;
+                this.moveListener = this.stage.on("stagemousemove", this.drag);
+            },
+            drag: function(event) {
+                this.r.w = event.stageX - this.selection.x;
+                this.r.h = event.stageY - this.selection.y;
+            },
+            dragEnd: function() {
+                this.stage.off("stagemousemove", this.moveListener);
+            },
         }
     }
-
-
-
-
 </script>
 
 <style scoped>
-
+    canvas {
+        border: 1px solid red;
+        width: 100%;
+    }
 </style>
