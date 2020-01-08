22aa87420e57a895ccadaba26f3bb66aec4ee0d9
Pavel Shvetsov <pshvetso@mail.ru>
2020-01-08 12:36:06 +0300

    Sidebar component implementation.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,38 +1,70 @@
 <template>
     <div>
         <ul class="sidebar navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span>
-                </a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span>
-                </a>
-            </li>
+            <template v-for="routes in links">
+                <div v-if="routes.header" class="sidebar-heading" v-bind:key="routes.id">
+                    {{routes.header}}
+                </div>
+                <router-link :to="routes.page" v-bind:key="routes.id" v-slot="{ href, isExactActive  }">
+                        <li class="nav-item" :class="[isExactActive && 'active']">
+                            <a :href="href" class="nav-link">
+                                <i class="fa-fw" :class="`${routes.class}`"></i>
+                                <span>{{ routes.text }}</span>
+                            </a>
+                        </li>
+                </router-link>
+                <hr v-if="routes.hr" class="sidebar-divider" v-bind:key="routes.id">
+            </template>
         </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Sidebar"
+        name: "Sidebar",
+        data: () => ({
+            links: [
+                {
+                    id: 0,
+                    text: 'My page',
+                    page: '/mypage',
+                    class: 'far fa-address-card',
+                },
+                {
+                    id: 1,
+                    text: 'Feed',
+                    page: '/',
+                    class: 'fal fa-badge-dollar',
+                    hr: true,
+                },
+                {
+                    id: 2,
+                    text: 'Today',
+                    page: { path: 'top', query: { interval: 'day' }},
+                    class: 'fas fa-angle-double-up',
+                    header: "Top",
+                },
+                {
+                    id: 3,
+                    text: 'Week',
+                    page: { path: 'top', query: { interval: 'week' }},
+                    class: 'fas fa-angle-double-up',
+                },
+                {
+                    id: 4,
+                    text: 'Month',
+                    page: { path: 'top', query: { interval: 'month' }},
+                    class: 'fas fa-angle-double-up',
+                },
+                {
+                    id: 5,
+                    text: 'All time',
+                    page: { path: 'top', query: { interval: 'alltime' }},
+                    class: 'fas fa-arrow-up',
+                    hr: true,
+                },
+            ]
+        }),
     }
 </script>
 
@@ -50,36 +82,14 @@
         margin-bottom: 1rem;
     }
 
-    .sidebar .nav-item .nav-link {
-        text-align: center;
-        padding: 0.75rem 1rem;
-        width: 90px;
+    .sidebar .nav-item:hover {
+        background-color: #ff253a !important;
     }
 
-    .sidebar .nav-item .nav-link span {
-        font-size: 0.65rem;
-        display: block;
-    }
-
-    .sidebar .nav-item .dropdown-menu {
-        position: absolute !important;
-        -webkit-transform: none !important;
-        transform: none !important;
-        left: calc(90px + 0.5rem) !important;
-        margin: 0;
-    }
-
-    .sidebar .nav-item .dropdown-menu.dropup {
-        bottom: 0;
-        top: auto !important;
-    }
-
-    .sidebar .nav-item.dropdown .dropdown-toggle::after {
-        display: none;
-    }
-
-    .sidebar .nav-item .nav-link {
-        color: #a1a0a0;
+    .sidebar .nav-item.active {
+        background-color: white;
+        border-left: 1px lightgray dashed;
+        border-right: 1px lightgray dashed;
     }
 
     .sidebar .nav-item.active .nav-link {
@@ -87,8 +97,36 @@
         font-weight: 500;
     }
 
-    .sidebar .nav-item .nav-link:active, .sidebar .nav-item .nav-link:focus, .sidebar .nav-item .nav-link:hover {
-        color: #707070;
+    .sidebar .nav-item .nav-link {
+        text-align: center;
+        padding: 0.75rem 1rem;
+        width: 90px;
+        color: #818181;
+    }
+
+    .sidebar .nav-item .nav-link:active,
+    .sidebar .nav-item .nav-link:focus,
+    .sidebar .nav-item .nav-link:hover {
+        color: white;
+    }
+
+    .sidebar .nav-item .nav-link span {
+        font-size: 0.65rem;
+        display: block;
+    }
+
+    .sidebar .nav-item.active .nav-link span,
+    .sidebar .nav-item .nav-link:active span {
+        color: black;
+    }
+
+    .sidebar .nav-item .nav-link i {
+        font-size: 1rem;
+        color: #bebebe;
+    }
+
+    .sidebar .nav-item.active .nav-link i {
+        color: black;
     }
 
     .sidebar.toggled {
@@ -96,9 +134,30 @@
         overflow: hidden;
     }
 
+    hr.sidebar-divider {
+        border-top: 1px solid lightgray;
+        margin: 0 1rem 1rem;
+        box-sizing: content-box;
+        height: 0;
+        overflow: visible;
+    }
+
+    .sidebar-heading {
+        color: #bebebe;
+        padding: 0 1rem;
+        font-weight: 800;
+        font-size: .65rem;
+        text-transform: uppercase !important;
+        text-align: center;
+    }
+
     @media (min-width: 768px) {
         .sidebar {
             width: 225px !important;
+        }
+
+        .sidebar.toggled .nav-item:last-child {
+            margin-bottom: 1rem;
         }
 
         .sidebar .nav-item .nav-link {
@@ -109,27 +168,18 @@
         }
 
         .sidebar .nav-item .nav-link span {
-            font-size: 1rem;
+            font-size: 0.85rem;
             display: inline;
+            padding-left: .7rem;
         }
 
-        .sidebar .nav-item .dropdown-menu {
-            position: static !important;
-            margin: 0 1rem;
-            top: 0;
-        }
-
-        .sidebar .nav-item.dropdown .dropdown-toggle::after {
-            display: block;
+        .sidebar .nav-item .nav-link i {
+            padding-left: .5rem;
         }
 
         .sidebar.toggled {
             overflow: visible;
             width: 90px !important;
-        }
-
-        .sidebar.toggled .nav-item:last-child {
-            margin-bottom: 1rem;
         }
 
         .sidebar.toggled .nav-item .nav-link {
@@ -143,21 +193,8 @@
             display: block;
         }
 
-        .sidebar.toggled .nav-item .dropdown-menu {
-            position: absolute !important;
-            -webkit-transform: none !important;
-            transform: none !important;
-            left: calc(90px + 0.5rem) !important;
-            margin: 0;
-        }
-
-        .sidebar.toggled .nav-item .dropdown-menu.dropup {
-            bottom: 0;
-            top: auto !important;
-        }
-
-        .sidebar.toggled .nav-item.dropdown .dropdown-toggle::after {
-            display: none;
+        .sidebar-heading {
+            text-align: left;
         }
     }
 
