898c3af1797c07c2146866545dfc5ee7b66ce680
Deepred <pshvetso@gmail.com>
2023-05-14 22:16:02 +0300

    Support two requests, feed and feedData, and joining response data on frontend.

--- src/mixins/CallApiMixin.vue
+++ src/mixins/CallApiMixin.vue
@@ -19,25 +19,59 @@
 
                 this.isApiCallPending = true;
 
-                //window.console.log("/api/top?interval=" + this.interval + "&page=" + this.page);
                 axios
                     .get(this.apiUrl)
                     .then(response => {
-                        this.isApiCallPending = false;
                         let data = response.data;
-                        //window.console.log(data);
 
-                        if (data.length > 0) {
+                        let len = data.length;
+
+                        if (len > 0) {
                             this.posts.push(...data);
-                            this.page++;
+                            this.callDataAPI();
+                            this.postId = data[len-1].id;
                         } else {
                             this.noMoreData = true;
                         }
+
+                        this.isApiCallPending = false;
                     })
                     .catch(error => {
                         this.errors.push(error);
                     });
             },
+            callDataAPI() {
+                axios
+                    .get(this.apiDataUrl)
+                    .then(response => {
+                        let data = response.data;
+
+                        let len = data.length;
+                        let postsLen = this.posts.length;
+
+                        if (len > 0) {
+                            for(var i = 1; i <= len; i++) {
+                                if(this.posts[postsLen - i].id != data[len - i].id) {
+                                    window.console.log("Posts.id not match data.id!");
+                                }
+                                // this.posts[postsLen - i].liked = data[len - i].liked;
+                                // merge data objects with same keys
+                                this.posts[postsLen - i] = Object.assign(
+                                    {},
+                                    this.posts[postsLen - i],
+                                    data[len - i]
+                                );
+                            }
+
+                            // update reactive data to force component update
+                            this.posts = this.posts.concat([]);
+                            window.console.log(this.posts);
+                        }
+                    })
+                    .catch(error => {
+                        this.errors.push(error);
+                    });
+            }
         },
     }
 </script>
